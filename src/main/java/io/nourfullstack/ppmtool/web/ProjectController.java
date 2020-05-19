48b77f11ebbd1b87fdde24ac22f140194b806023
laxdanny <aboodi.nour@gmail.com>
2020-05-20 00:33:47 +0400

    find All project

--- src/main/java/io/nourfullstack/ppmtool/web/ProjectController.java
+++ src/main/java/io/nourfullstack/ppmtool/web/ProjectController.java
@@ -35,5 +35,10 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProject(){
+        return projectService.findAllProjects();
+    }
 }
  
